7050bcca167ef037854b91b53e34fab3fa387e06
AlexST <mymail@ya.ru>
2022-12-12 16:27:59 +0300

    Задание 42 готово

---
+++ DZ5_ZAD42.py
@@ -0,0 +1,33 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+def identical(line, i, s):
+    count = 2
+    while (i + count < len(line)) and (s == line[i+count]):
+        count += 1
+
+    print(type(str(count) + s))
+    out_file.write(str(count) + s)
+    return i + count
+
+
+def not_identical(line, i, s):
+    count = -2
+    while (i +(-count) < len(line)) and (s != line[i+(-count)]):
+        count -= 1
+
+    out_file.write(''.join(str(count) + line[i:i+(-count)]))
+    return i +(-count)
+
+with open("input.txt", "r") as in_file:
+    with open("output.txt", "w") as out_file:
+
+        for line in in_file:
+            i=0
+            count = 0
+            while i < len(line)-1:
+                s = line[i]
+                if s == '\\': break
+                if s == line[i+1]:
+                    i = identical(line, i, s)
+                else:
+                    i = not_identical(line, i, s)
